f0a35c0255d5c3c6d411a9e1b3bae7f2edec499e
KPalianok <KPalianok@gomel.iba.by>
2025-04-07 18:29:26 +0300

    Deleting HW 10 files.

--- javaLessons/src/main/java/Lesson_10/Apple.java
+++
@@ -1,7 +0,0 @@
-package Lesson_10;
-
-public class Apple extends Fruit {
-	public Apple() {
-		super(1.0f);
-	}
-}
--- javaLessons/src/main/java/Lesson_10/Orange.java
+++
@@ -1,7 +0,0 @@
-package Lesson_10;
-
-public class Orange extends Fruit {
-	public Orange() {
-		super(1.5f);
-	}
-}
